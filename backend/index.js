2363666ebf9815699c47c33f0690b07f8a84ca90
Khushal Kumar <khushalbhatt17@gmail.com>
2025-03-11 00:15:50 +0530

    First Changes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,36 @@
-const express = require("express");
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { userModel } from './root/db.js';
+const app = express();
 
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get("/", (req,res) => {
+    res.send("Hello World");
+});
+
+app.post('/signup',async (req,res)=>{
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const password = req.body.password;
+    
+    const abc = await userModel.findOne({firstname: firstname, lastname: lastname});
+    if(abc){
+        res.status(400).send("User already exists");
+        return;
+    }
+    await userModel.create({
+        firstname: firstname,
+        lastname: lastname,
+        password: password,
+    })
+    res.status(201).send("User Created");
+})
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
---
+++ backend/root/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://Khushalbhatt:Khushal%40123@cluster0.ytwue.mongodb.net/paytm');
+
+const userSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    password: String,
+})
+
+const userModel = mongoose.model('users', userSchema);
+
+export { userModel };
